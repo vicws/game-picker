946ff79275def1091f25157776e023e6a690ad5a
Victor Westphal <vfwestphal@gmail.com>
2023-11-28 11:52:26 -0300

    added an ifelse statement just for gits and shiggles

--- dinner-picker.js
+++ dinner-picker.js
@@ -1,8 +1,12 @@
-const dinner = ["Sushi", "Pizza", "Mexican", "Burgers", "Unlimited Breadsticks and Salad"]
+const dinner = ["Sushi", "Pizza", "Mexican", "Burgers", "Unlimited Breadsticks and Salad", "Kebabs"]
 function pickDinner(dinner) {
 
     let randomSelection = Math.floor(Math.random() * dinner.length);
-    alert(`Alright, go put your make-up on; we're going out for ${dinner[randomSelection]}!`)
+    if (randomSelection == dinner.length - 1) {
+        alert(`TONIGHT, WE SHALL FEAST ON THE SOULS OF THE UNWORTHY!!! ...*cough* sorry, I meant \"Babe, we're going out for kebabs\"`);
+    } else {
+        alert(`Alright, go put your make-up on; we're going out for ${dinner[randomSelection]}!`);
+    }
 
 }
 pickDinner(dinner);
